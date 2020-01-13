400133d2eec123abd30beb1f11bbd29a717ccdfd
Aidan Shea <aidanshea1@gmail.com>
2020-01-13 17:39:15 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Aidan Shea on 1/13/20.
+//  Copyright © 2020 Aidan Shea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
